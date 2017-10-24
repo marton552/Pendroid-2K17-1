2a8be6b56b118367769985dc22741021e88e25ff
666420 <asd6666@mailfence.com>
2017-10-24 16:21:14 +0200

    Matekos rész majdnem kész

--- core/src/com/hakkatoreinbukuma/game/Core.java
+++ core/src/com/hakkatoreinbukuma/game/Core.java
@@ -1,15 +1,16 @@
 package com.hakkatoreinbukuma.game;
 
 public class Core {
+	public static float grav = 9.81f;
 	public static float[] calcAngle(float x, float y, float v0){
-      		float grav = 9.81f;
       		double gyok = Math.sqrt(Math.pow(v0, 4)-grav*(grav*(x*x)+(2*y*(v0*v0))));
         	float megoldas1 = (float)Math.atan(((v0*v0)-gyok)/(grav*x)) * (float)(180f/Math.PI);
       		float megoldas2 = (float)Math.atan(((v0*v0)+gyok)/(grav*x)) * (float)(180f/Math.PI);
     		return new float[] {megoldas1, megoldas2};
     	}
-    	public float[] getArc(){
-		float[] arc = {1.0f, 1.0f};
-        	return arc;
-    	}
+	public static float calcHeight(float x, float angle, float v0){
+		angle = (float)Math.PI/180f*angle;
+		float height = (float)(x*((-1f*grav)*(Math.tan(angle)*Math.tan(angle))+2f*(v0*v0)*Math.tan(angle)-(grav*x)))/(2f*(v0*v0));
+		return height;
+	}
 }
