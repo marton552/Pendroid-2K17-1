e2b0756f6af238d4436d8d156d255f402cb2938b
hormartin <pigfighter2@gmail.com>
2017-10-25 10:26:36 +0200

    Touc event GameStagebe

---
+++ core/src/com/hakkatoreinbukuma/game/Bullet.java
@@ -0,0 +1,11 @@
+package com.hakkatoreinbukuma.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.hakkatoreinbukuma.game.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+
+public class Bullet extends OneSpriteStaticActor {
+
+    public Bullet(Texture texture) {
+        super(texture);
+    }
+}
